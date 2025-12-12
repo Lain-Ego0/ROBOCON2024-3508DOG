ccb0d7f66ac17befe9db3aa6dd604ad81f3a2ea1
Lain-Ego0 <2302109068@qq.com>
2025-12-12 08:52:03 +0800

    Delete Useless file

--- 2.software/MCU/Robocon_F427/Objects/packet.d
+++
@@ -1,6 +0,0 @@
-.\objects\packet.o: ..\MTI_USER\packet.c
-.\objects\packet.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\packet.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\packet.o: ..\MTI_USER\packet.h
-.\objects\packet.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\packet.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
